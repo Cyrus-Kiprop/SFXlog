80a9f627fd3435adb23c7adf46ab4414e67e5c89
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-11-23 10:19:56 +0300

    configure source-file-system && mdx plugins

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  siteMetadata: {
+    title: `The Localhost Blog`,
+    description: `This is my coding blog where I write about my coding journey.`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+  ],
+};
