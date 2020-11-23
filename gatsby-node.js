8502fb48ff808ca5d164ade5a6d1b9ac1464e487
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-11-23 13:32:40 +0300

    create a page for each of mdx node

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,3 +12,38 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     });
   }
 };
+
+// for each and every node that exist create a page file and attach the slug url
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve("src/templates/blogPostTemplate.js");
+  return graphql(`
+    {
+      allMdx {
+        nodes {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    if (result.errors) {
+      throw result.errors;
+    }
+    const posts = result.data.allMdx.nodes;
+    // create page for each mdx file
+    posts.forEach((post) => {
+      createPage({
+        path: post.fields.slug,
+        component: blogPostTemplate,
+        context: {
+          slug: post.fields.slug,
+        },
+      });
+    });
+  });
+};
