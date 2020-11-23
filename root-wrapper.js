b5bc451a416cdd4b2a65a2a0a76a0cf5d8c2bc6b
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-11-23 15:56:08 +0300

    configure roowrapper element for codeblocks

---
+++ root-wrapper.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { MDXProvider } from "@mdx-js/mdx";
+
+const components = {
+  h2: ({ children }) => <h2 style={{ color: "rebeccapurple" }}>{children}</h2>,
+  "p.inlineCode": (props) => (
+    <code style={{ backgroundColor: "lightgray" }} {...props} />
+  ),
+};
+
+export const wrapRootElement = ({ element }) => (
+  <MDXProvider components={components}>{element}</MDXProvider>
+);
