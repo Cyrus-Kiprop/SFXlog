a9e34fbb83a10e15297f4164975b2197db5160de
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-11-23 11:01:05 +0300

    style the header using styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 import { useSiteMetadata } from "../hooks/useSiteMetadata";
+
+const StyledH1 = styled.h1`
+  color: rebeccapurple;
+`;
 
 export default function Index() {
   const { title, description } = useSiteMetadata();
   return (
     <div>
-      <h1> {title}</h1>
+      <StyledH1> {title}</StyledH1>
       <p> {description}</p>
     </div>
   );
