8962268183430f8e03d29e5158d916ccc8eec257
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-11-23 14:04:23 +0300

    add previous and next context while creating pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -20,7 +20,10 @@ exports.createPages = ({ actions, graphql }) => {
   const blogPostTemplate = path.resolve("src/templates/blogPostTemplate.js");
   return graphql(`
     {
-      allMdx {
+      allMdx(
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: { frontmatter: { published: { eq: true } } }
+      ) {
         nodes {
           fields {
             slug
@@ -37,7 +40,9 @@ exports.createPages = ({ actions, graphql }) => {
     }
     const posts = result.data.allMdx.nodes;
     // create page for each mdx file
-    posts.forEach((post) => {
+    posts.forEach((post, index, posts) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1];
+      const next = index === 0 ? null : posts[index - 1];
       createPage({
         path: post.fields.slug,
         component: blogPostTemplate,
