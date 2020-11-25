80e6daf7292f0fa4e9b8f5d4518f00c0874b458d
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-11-25 09:06:18 +0300

    configure images plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,8 @@ module.exports = {
   siteMetadata,
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
@@ -28,6 +30,22 @@ module.exports = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+        ],
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+        ],
       },
     },
     {
